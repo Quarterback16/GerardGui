45d2a8dc9a94b7090a512670ff43b6144c5cedea
Quarterback16 <quarterback16@grapevine.com.au>
2015-10-28 18:36:44 +1100

    trying to sync

---
+++ RosterLib/Interfaces/IGetGamebooks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RosterLib.Interfaces
+{
+   public interface IGetGamebooks
+   {
+      int DownloadWeek(NFLWeek week);
+
+      string Seed(NFLWeek week);
+   }
+}
