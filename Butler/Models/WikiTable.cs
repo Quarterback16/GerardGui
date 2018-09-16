bb9ac48f895ceabc834ee5c5985801eb41795864
Quarterback16 <quarterback16@grapevine.com.au>
2018-09-16 16:49:51 +1000

    More stuff for the wiki table

--- Butler/Models/WikiTable.cs
+++ Butler/Models/WikiTable.cs
@@ -34,6 +34,7 @@ namespace Butler.Models
                 "WHEN",
                 "MU",
                 "PRO",
+                "ACT",
                 "QB",
                 "QBA",
                 "RB",
@@ -70,8 +71,17 @@ namespace Butler.Models
         {
             game.CalculateSpreadResult();
             LoadUnits(game);
-            AddAwayRow(game);
-            AddHomeRow(game);
+            AddTeamRow(
+                game: game,
+                team: game.AwayNflTeam,
+                projectedScore: game.BookieTip.AwayScore,
+                actualScore: game.AwayScore);
+            AddTeamRow(
+                game: game,
+                team: game.HomeNflTeam,
+                projectedScore: game.BookieTip.HomeScore,
+                actualScore: game.HomeScore,
+                showWhen: false);
             AddBlankRow();
         }
 
@@ -98,39 +108,51 @@ namespace Butler.Models
             InternalTable.Body.Rows.Add(dr);
         }
 
-        private void AddAwayRow(NFLGame game)
+        private void AddTeamRow(
+            NFLGame game,
+            NflTeam team,
+            int projectedScore,
+            int actualScore,
+            bool showWhen = true)
         {
             var dr = InternalTable.Body.NewRow();
-            dr["WHEN"] = TimeBit(game);
-            dr["MU"] = game.AwayNflTeam.TeamCode;
-            dr["PRO"] = game.BookieTip.AwayScore;
+            if (showWhen)
+                dr["WHEN"] = TimeBit(game);
+            dr["MU"] = team.TeamCode;
+            dr["PRO"] = ProjectedTeamScore(projectedScore);
+            if (game.Played() )
+                dr["ACT"] = ActualTeamScore(actualScore);
             dr["QB"] = PickupChart.PlayerWikiPiece(
-                game.AwayNflTeam.PassUnit.Q1,
+                team.PassUnit.Q1,
                 game,
                 YahooCalculator);
             var wikiTeam = new WikiTeam(
-                game.AwayNflTeam,
+                team,
                 game);
+            dr["QBA"] = PickupChart.ActualOutput(
+                game: game,
+                player: team.PassUnit.Q1,
+                isReport:false);
             SetMainTeamRoles(dr, wikiTeam);
-
             InternalTable.Body.Rows.Add(dr);
         }
 
-        private void AddHomeRow(NFLGame game)
+        private static string ProjectedTeamScore(int score)
         {
-            var dr = InternalTable.Body.NewRow();
-            dr["WHEN"] = "    ";
-            dr["MU"] = game.HomeNflTeam.TeamCode;
-            dr["PRO"] = game.BookieTip.HomeScore;
-            dr["QB"] = PickupChart.PlayerWikiPiece(
-                game.HomeNflTeam.PassUnit.Q1,
-                game,
-                YahooCalculator);
-            var wikiTeam = new WikiTeam(
-                game.HomeNflTeam,
-                game);
-            SetMainTeamRoles(dr, wikiTeam);
-            InternalTable.Body.Rows.Add(dr);
+            return TeamScore(score,24);
+        }
+
+        private static string ActualTeamScore(int score)
+        {
+            return TeamScore(score, 34);
+        }
+
+        private static string TeamScore(int score, int limit)
+        {
+            if (score > limit)
+                return $"**{score}**";
+
+            return $"{score}";
         }
 
         private void SetMainTeamRoles(DataRow dr, WikiTeam wikiTeam)
@@ -138,26 +160,46 @@ namespace Butler.Models
             dr["RB"] = PickupChart.GetRunnerWikiBit(
                 team: wikiTeam,
                 calculator: YahooCalculator);
+            dr["RBA"] = PickupChart.ActualOutput(
+                game: wikiTeam.Game,
+                player: wikiTeam.Team.RunUnit.AceBack,
+                isReport: false);
             dr["W1"] = PickupChart.GetPlayerWikiBit(
                 wikiTeam.Team.PassUnit.W1,
                 team: wikiTeam,
                 calculator: YahooCalculator,
                 bLinks: false);
+            dr["W1A"] = PickupChart.ActualOutput(
+                game: wikiTeam.Game,
+                player: wikiTeam.Team.PassUnit.W1,
+                isReport: false);
             dr["W2"] = PickupChart.GetPlayerWikiBit(
                 wikiTeam.Team.PassUnit.W2,
                 team: wikiTeam,
                 calculator: YahooCalculator,
                 bLinks: false);
+            dr["W2A"] = PickupChart.ActualOutput(
+                game: wikiTeam.Game,
+                player: wikiTeam.Team.PassUnit.W2,
+                isReport: false);
             dr["TE"] = PickupChart.GetPlayerWikiBit(
                 wikiTeam.Team.PassUnit.TE,
                 team: wikiTeam,
                 calculator: YahooCalculator,
                 bLinks: false);
+            dr["TEA"] = PickupChart.ActualOutput(
+                game: wikiTeam.Game,
+                player: wikiTeam.Team.PassUnit.TE,
+                isReport: false);
             dr["PK"] = PickupChart.GetPlayerWikiBit(
                 wikiTeam.Team.KickUnit.PlaceKicker,
                 team: wikiTeam,
                 calculator: YahooCalculator,
                 bLinks: false);
+            dr["PKA"] = PickupChart.ActualOutput(
+                game: wikiTeam.Game,
+                player: wikiTeam.Team.KickUnit.PlaceKicker,
+                isReport: false);
         }
 
         private static string TimeBit(NFLGame game)
@@ -184,11 +226,37 @@ namespace Butler.Models
             header.Append("||");
             foreach (var column in Cols)
             {
-                header.Append(" **");
-                header.Append(column);
-                header.Append("** ||");
+                if (IsActualColumn(column))
+                    header.Append("||");
+                else
+                {
+                    if (IsPosition(column))
+                    {
+                        header.Append($"|| **{column}** ");
+                    }
+                    else
+                        header.Append($" **{column}** ||");
+                }
             }
             return header.ToString();
+        }
+
+        private static bool IsPosition(string column)
+        {
+            if (column == "QB"
+                || column == "RB"
+                || column == "W1"
+                || column == "W2"
+                || column == "TE"
+                || column == "PK")
+                return true;
+            return false;
+        }
+
+        private static bool IsActualColumn(string column)
+        {
+            var cLastCharacter = column[column.Length - 1];
+            return (cLastCharacter == 'A');
         }
 
         private string DataRowToWiki(DataRow dr)
--- Gerard.Tests/GamebookGetterTests.cs
+++ Gerard.Tests/GamebookGetterTests.cs
@@ -11,7 +11,7 @@ namespace Gerard.Tests
 		[TestMethod]
 		public void TestGetGamebooksForCurrentWeek()
 		{
-			const string weekToDownload = "01";
+			const string weekToDownload = "02";
 
 			var week = new NFLWeek(
                 seasonIn: "2018",
