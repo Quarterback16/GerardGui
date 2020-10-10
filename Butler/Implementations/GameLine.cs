4e157e4034937c38fa6c75d4bb90fcc5f4a34f2e
Steve Colonna <quarterback16@grapevine.com.au>
2020-10-10 15:10:29 +1100

    built LineMaster that returns nothing

--- Butler/Implementations/GameLine.cs
+++ Butler/Implementations/GameLine.cs
@@ -5,5 +5,11 @@ namespace Butler.Implementations
     {
         public decimal Spread { get; set; }
         public decimal Total { get; set; }
+
+        public GameLine()
+        {
+            Spread = 0.0M;
+            Total = 0.0M;
+        }
     }
 }
--- Butler/Implementations/LineMaster.cs
+++ Butler/Implementations/LineMaster.cs
@@ -1,8 +1,16 @@
 using Butler.Interfaces;
+using System;
 
 namespace Butler.Implementations
 {
     public class LineMaster : ILineMaster
     {
+        public GameLine GetLine(
+            DateTime gameDate,
+            string homeTeamCode)
+        {
+            var result = new GameLine();
+            return result;
+        }
     }
 }
